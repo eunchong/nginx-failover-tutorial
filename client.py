ef81c76fb1197701db6df6556ab8d333347b493b
Eunchong Lee <gr4ce@korea.ac.kr>
2019-08-12 03:10:44 +0900

    Add code for non_idempotent case

--- client.py
+++ client.py
@@ -2,10 +2,14 @@ import requests
 import time
 import os
 HOST = os.getenv('HOST', 'localhost')
+METHOD = os.getenv('METHOD', 'get')
 
 for idx in range(10000):
     try:
-        res = requests.get("http://%s/healthcheck"%(HOST)).json()
+        if METHOD == 'get':
+            res = requests.get("http://%s/healthcheck"%(HOST)).json()
+        else:
+            res = requests.post("http://%s/healthcheck"%(HOST)).json()
         print(res)
         time.sleep(0.5)
     except:
